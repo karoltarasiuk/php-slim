8298c4a9e4a221a7cb9c564ab2516931c56772cc
Karol Tarasiuk <karoltarasiuk@gmail.com>
2016-09-26 18:32:24 +1000

    PDO wrapper class added to handle all most common database operations.

--- code/server/latest/src/database.php
+++ code/server/latest/src/database.php
@@ -1,8 +1,41 @@
 <?php
 
+/**
+ * PDO wrappper class to handle some most common database operations.
+ */
 class Database {
 
+    /**
+     * Gets PDO adapter.
+     *
+     * @return PDO
+     */
     public static function getPdo() {
         return Application::getSlimAppContainer()->db;
+    }
+
+    /**
+     * Returns prepared PDO statement as a result of execution of SQL query.
+     *
+     * @param  string $sql
+     * @param  array  $args
+     * @return PDOStatement
+     */
+    public static function execute($sql, $args = null) {
+        $pdo = self::getPdo();
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute($args);
+        return $stmt;
+    }
+
+    /**
+     * Returns data after fetching all matching rows.
+     *
+     * @param  string $sql
+     * @param  array  $args
+     * @return array
+     */
+    public static function fetchAll($sql, $args = null) {
+        return self::execute($sql, $args)->fetchAll();
     }
 }
